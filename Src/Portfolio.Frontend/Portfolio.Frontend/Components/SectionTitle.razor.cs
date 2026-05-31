448866a5e5bb170af194577fd408c9e3b375d021
MauroL-AllPhi <mauro.leonardo.04@outlook.com>
2026-05-31 16:53:22 +0200

    Updated home & about page to fix spacing issue. Updated button default colors

--- Src/Portfolio.Frontend/Portfolio.Frontend/Components/SectionTitle.razor.cs
+++ Src/Portfolio.Frontend/Portfolio.Frontend/Components/SectionTitle.razor.cs
@@ -6,5 +6,6 @@ namespace Portfolio.Frontend.Components;
 public partial class SectionTitle : ComponentBase
 {
     [Parameter] public Color Color { get; set; } = Color.Inherit;
+    [Parameter] public bool Center { get; set; }
     [Parameter] public RenderFragment? ChildContent { get; set; }
 }
