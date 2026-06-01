68a88f9f273459ef5e819645ffa6f2553ff5941c
MauroL-AllPhi <mauro.leonardo.04@outlook.com>
2026-06-01 15:52:04 +0200

    Created base contact form without logic

---
+++ Src/Portfolio.Frontend/Portfolio.Frontend/Pages/Contact.razor.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Components;
+
+namespace Portfolio.Frontend.Pages;
+
+public partial class Contact : ComponentBase
+{
+    private string _name = string.Empty;
+    private string _subject = string.Empty;
+    private string _email = string.Empty;
+    private string _message = string.Empty;
+}
