9dd88f4edccb054a0ee542a7702e2ddc32235ce7
MauroL-AllPhi <mauro.leonardo.04@outlook.com>
2026-06-03 10:00:16 +0200

    Updated route and 404 and index.html

--- Src/Portfolio.Frontend/Portfolio.Frontend/Constants/AppRoutes.cs
+++ Src/Portfolio.Frontend/Portfolio.Frontend/Constants/AppRoutes.cs
@@ -4,12 +4,12 @@ public static class AppRoutes
 {
     private const string _baseUrl = "/MauroLeonardoPortfolio";
     
-    public const string AboutUrl = _baseUrl + "/about-me";
-    public const string ContactUrl = _baseUrl + "/contact";
-    public const string SkillsetUrl = _baseUrl + "/skillset";
-    public const string HomeUrl = _baseUrl;
-    public const string InternshipsUrl = _baseUrl + "/internships";
-    public const string ProjectsUrl = _baseUrl + "/projects";
+    public const string AboutUrl =  $"{_baseUrl}/about-me";
+    public const string ContactUrl = $"{_baseUrl}/contact";
+    public const string SkillsetUrl = $"{_baseUrl}/skillset";
+    public const string HomeUrl = $"{_baseUrl}";
+    public const string InternshipsUrl = $"{_baseUrl}/internships";
+    public const string ProjectsUrl = $"{_baseUrl}/projects";
 
     public const string InternshipTctUrl = "tct";
     public const string InternshipCmsUrl = "cms";
@@ -18,5 +18,5 @@ public static class AppRoutes
     public const string ProjectHotelKempenrustUrl = "hotel-kempenrust";
     public const string ProjectTtcWestelUrl = "ttc-westel";
 
-    public static string ProjectDetailUrl(string projectName) => $"/{ProjectsUrl}/{projectName}";
+    public static string ProjectDetailUrl(string projectName) => $"{_baseUrl}/projects/{projectName}";
 }
